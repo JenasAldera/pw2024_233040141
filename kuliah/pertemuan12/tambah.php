67f8020365f8e839d53072fcae56742782f84a06
Jenas <jenasaldera1425@gmail.com>
2024-05-29 01:36:16 +0700

    .

---
+++ kuliah/pertemuan12/tambah.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data Mahasiswa</title>
+</head>
+<body>
+    
+    <h1>Tambah Data Mahasiswa</h1>
+
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="nrp">NRP : </label>
+                <input type="text" name="nrp" id="nrp">
+            </li>
+            <li>
+                <label for="nama">Nama : </label>
+                <input type="text" name="nama" id="nama">
+            </li>
+            <li>
+                <label for="emial">Email : </label>
+                <input type="text" name="email" id="email">
+            </li>
+            <li>
+                <label for="Jurusan">Jurusan : </label>
+                <input type="text" name="jurusan" id="jurusan">
+            </li>
+            <li>
+                <label for="gambar">Gambar : </label>
+                <input type="text" name="gambar" id="gambar">
+            </li>
+            <li>
+                <button>Submit</button>
+            </li>
+        </ul>
+
+    </form>
+
+</body>
+</html>
